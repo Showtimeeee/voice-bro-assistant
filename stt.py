8abb44b6b82f574119188d709c88f7fb4dc83cb9
Vadim <94108043+Showtimeeee@users.noreply.github.com>
2026-05-19 23:29:07 +0300

    Update stt.py

--- stt.py
+++ stt.py
@@ -1,25 +1,37 @@
 import vosk
-import wave
 import json
+import pyaudio
 import config
+
 
 class SpeechToText:
     def __init__(self):
         self.model = vosk.Model(config.VOSK_MODEL_PATH)
-        self.rec = vosk.Recognizer()
-        
+        self.CHUNK = 4000
+        self.FORMAT = pyaudio.paInt16
+        self.CHANNELS = 1
+        self.RATE = 16000
+
     def listen(self):
-        with wave.open("input.wav", "rb") as wf:
-            if wf.getnchannels() != 1 or wf.getsampwidth() != 2 or wf.getcomptype() != "NONE":
-                raise ValueError("Неверный формат аудио")
-                
+        rec = vosk.KaldiRecognizer(self.model, self.RATE)
+        p = pyaudio.PyAudio()
+        stream = p.open(
+            format=self.FORMAT,
+            channels=self.CHANNELS,
+            rate=self.RATE,
+            input=True,
+            frames_per_buffer=self.CHUNK,
+        )
+
+        try:
             while True:
-                data = wf.readframes(4000)
-                if len(data) == 0:
-                    break
-                if self.rec.AcceptWaveform(data):
-                    result = json.loads(self.rec.Result())
-                    if result['text']:
-                        return result['text']
-            result = json.loads(self.rec.FinalResult())
-            return result['text']
+                data = stream.read(self.CHUNK, exception_on_overflow=False)
+                if rec.AcceptWaveform(data):
+                    result = json.loads(rec.Result())
+                    text = result.get("text", "")
+                    if text:
+                        return text
+        finally:
+            stream.stop_stream()
+            stream.close()
+            p.terminate()
